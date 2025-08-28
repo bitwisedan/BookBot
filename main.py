9c1f78e3179a530fac5b0d3095bdc1d03311685b
bitwisedan <thebitwisedan@gmail.com>
2025-08-28 16:07:45 -0400

    working on sort report

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import num_of_words
+from stats import num_of_characters
+def get_book_text(filepath):
+    with open(filepath) as file:
+        return file.read()
+
+def main():
+    filepath = "books/frankenstein.txt" 
+    characters = num_of_characters(get_book_text(filepath))
+    print(f"{num_of_words(get_book_text(filepath))} words found in the document")
+    print(characters)
+
+main()
