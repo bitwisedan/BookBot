fde730d3e88c8eeb96efcaa8c059237b19041e67
bitwisedan <thebitwisedan@gmail.com>
2025-09-02 10:15:32 -0400

    bookbot complete

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from stats import num_of_words
 from stats import num_of_characters
 from stats import sort_on
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
         return file.read()
 
 def main():
-    filepath = "books/frankenstein.txt" 
+    if sys.argv.__len__() < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
     characters = num_of_characters(get_book_text(filepath))
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
--- stats.py
+++ stats.py
@@ -1,11 +1,11 @@
 def num_of_words(text):
     words = text.split()
     return len(words)
-def num_of_characters(text):
-    letters = {'æ':0, 'â':0, 'ê':0, 'ë':0, 'ô':0, 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+def num_of_characters(text: str) -> dict:
+    letters = {}
     for char in text.lower():
-        if char in letters:
-            letters[char] += 1
+        if char.isalpha():
+            letters[char] = letters.get(char, 0) + 1
     return letters
 def sort_on(letters):
     sorted_list = []
